e81b5c8477d0d57a50b399d8024f498b35f10012
faq322 <34307377+faq322@users.noreply.github.com>
2023-04-27 16:00:23 +0300

    bet css

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -3,7 +3,7 @@ import Hand from "../Hand/Hand";
 import Wallet from "../Wallet/Wallet";
 import {RootStoreContext} from "../../App";
 import HandActions from "../Hand/HandActions/HandActions";
-import Bet from "../Wallet/Bet";
+import Bet from "../Wallet/Bet/Bet";
 import "./Player.css";
 import {observer} from "mobx-react";
 import PlayerName from "./PlayerName/PlayerName";
@@ -17,7 +17,7 @@ const Player: React.FC = () => {
             <div className={"player-hands-container"}>
                 {rootStore.handManagerStore.hands.map((hand, i) => {
                     return (<div key={i}>
-                        <Bet handStore={hand}></Bet>
+                        <Bet key={hand.betStore.getBet} handStore={hand}></Bet>
                         <Hand handStore={hand} owner={"player"}/>
                         <HandActions handStore={hand}/>
                     </div>);
---
+++ src/components/Wallet/Bet/Bet.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {observer} from "mobx-react";
+import HandStore from "../../../stores/HandStore";
+import "./Bet.css";
+import {HandStatus} from "../../../utils/Constant";
+
+type betProps = {
+    handStore: HandStore;
+};
+const Bet: React.FC<betProps> = ({handStore}) => {
+    const classes = [];
+    if (handStore.betStore.getBet > 0) classes.push('bet-green-anim');
+    if (handStore.getStatus() === HandStatus.Tie) classes.push('bet-tie');
+    if (handStore.getStatus() === HandStatus.Win) classes.push('bet-win');
+    if (handStore.getStatus() === HandStatus.Lost) classes.push('bet-lost');
+
+    return (
+        <>
+            <div className={`bet-container ${classes.join(' ')}`}>
+                <p>bet: {handStore.betStore.getBet}</p>
+            </div>
+        </>
+    );
+}
+
+export default observer(Bet);
